e8865ccd18a5aa3b85b08b9bd18075f923f1b823
RensvdMeijs <rensvdmeijs@gmail.com>
2021-06-21 00:48:27 +0200

    versie 2

--- composetest/fietsappV2.py
+++ composetest/fietsappV2.py
@@ -4,7 +4,7 @@ from bson import json_util
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 from pprint import pprint
 
-
+BikeId = 354125424
 ## download jsons
 # Creeër topics route voor consumer
 # Initialiseer consumer voor de fiets
@@ -12,48 +12,39 @@ from pprint import pprint
 tp = TopicPartition('route',0)
 bike_consumer = KafkaConsumer(
     bootstrap_servers="localhost:9092",
-    auto_offset_reset='earliest',
+    auto_offset_reset='latest',
     value_deserializer=lambda m:
         json.loads(m.decode('utf-8')))
 bike_consumer.assign([tp])
 bike_consumer.seek_to_beginning(tp)
 lastOffset = bike_consumer.end_offsets([tp])[tp]
+
+
+# De volgende Try consumeert de lijst dat een route moet zijn
+value_for_json = ""
 try:
     for message in bike_consumer:
-        pprint(message)
         if message.offset == lastOffset -1:
+            value_for_json = message.value
+            if (value_for_json['BikeId'] == BikeId):
+                with open('output.json', 'w') as json_file:
+                    json.dump(value_for_json, json_file)
             break
 except KeyboardInterrupt:
     pass
 finally:
     bike_consumer.close()
 
-# with open('output.json', 'w') as json_file:
-#     json.dump(value_for_json, json_file)
-
 ## laadt jsons
-json_roy_object = open('royjson.json')
+json_roy_object = open('output.json')
 json_roy_data = json.load(json_roy_object)
+
 json_route_object = open('route.json')
 json_route_data = json.load(json_route_object)
-print(type(json_route_data))
-# Eens voor testen
-# producer =KafkaProducer(bootstrap_servers='localhost:9092')
-# producer.send('route', json.dumps(json_roy_data, default=json_util.default).encode('utf-8'))
 
-# a -> b -> c
-# abc = route als json
-# bezienswaardigheden route = abc locaties als json
-# bikes, logs, route
-# bij bikes, laat ik weten waar ik ben geweest
-# bij logs, hetzelfde maar voor altijd
-# route krijg ik binnen nadat ik vraag om een route
-    # 
 #zet producer op KAFKA connectie
-bike_producer = KafkaProducer(
-    bootstrap_servers="localhost:9092")
+bike_producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
-BikeId = 354125424
 class Bicycle:
     def __init__(self, BikeId, X, Y):
         self.BikeId = BikeId
@@ -73,12 +64,30 @@ class Bicycle:
 
 ## Initieër json imports voordat je hier bent gekomen
 # Start locatie halen we uit een route bestand
-# bicycle
 start_locatie_X = json_roy_data['X']
 start_locatie_Y = json_roy_data['Y']
 bicycle = Bicycle(BikeId=BikeId, X= start_locatie_X, Y=start_locatie_Y)
 bicycle_json = json.dumps(bicycle.__dict__)
-pprint(bicycle_json)
+
+def printloc():
+    print(
+            "\tNaam:\t", 
+            place_name, " \n\tInfo:\t",
+            place_descriptie, " \n\tGenre:\t", 
+            place_genre, "\n\tbij coordinaten\t",
+            place_X, place_Y,"\n")
+print("\n\nU bent op X: ", start_locatie_X,"Y: ", start_locatie_Y)
+print("Te bezoeken plaatsen van uw route:")
+for place in json_roy_data['locations']:
+    place_X = place['X']
+    place_Y = place['Y']
+    place_descriptie = place['Info']
+    place_name = place['Name']
+    place_genre = place['Genre']
+    printloc()
+
+print("wilt u beginnen met uw route?\n Voer dan c in en druk op ENTER")
+breakpoint()
 
 print("\n\tBegin route")
 # route afleggen en locaties printen
@@ -91,10 +100,9 @@ for step in json_route_data['locaties']:
     bicycle.movement(X=step_X, Y=step_Y)
     sleep(3)
 
-    # print("hello")
     locations_here = []
     locations_around = []
-    for place in json_roy_data['locaties']:
+    for place in json_roy_data['locations']:
         place_X = place['X']
         place_Y = place['Y']
         # if statement, creeërt locaties op de huidige locatie
@@ -105,37 +113,20 @@ for step in json_route_data['locaties']:
             locations_around.append(place)
     print("Te bezoeken plaatsen waar u nu bent:\n\t")  
     for place in locations_here:
-        place_descriptie = place['discription']
-        place_name = place['name']
-        place_genre = place['genre']
+        place_descriptie = place['Info']
+        place_name = place['Name']
+        place_genre = place['Genre']
         print(
-            place_descriptie, " \n\t", 
-            place_name, " \n\t",
-            place_genre)
+            "\tNaam:\t", 
+            place_name, " \n\tInfo:\t",
+            place_descriptie, " \n\tGenre:\t", 
+            place_genre,"\n")
     print("plaatsen op uw route:")
     for place in locations_around:
-        place_descriptie = place['discription']
-        place_name = place['name']
-        place_genre = place['genre']
+        place_descriptie = place['Info']
+        place_name = place['Name']
+        place_genre = place['Genre']
         place_X = place['X']
         place_Y = place['Y']
-        print(
-            "\t", 
-            place_descriptie, " \n\t", 
-            place_name, " \n\t",
-            place_genre, "\n\tbij coordinaten",
-            place_X, place_Y)
-
-print("Te bezoeken plaatsen van uw route:")
-for place in json_roy_data['route']:
-    place_X = place['X']
-    place_Y = place['Y']
-    place_descriptie = place['discription']
-    place_name = place['name']
-    place_genre = place['genre']
-    # print(
-    #     #"Te bezoeken plaatsen van uw route:\n\t", 
-    #     "\t",place_descriptie, " \n\t", 
-    #     place_name, " \n\t", 
-    #     place_genre, "\n\t\tbij coordinaten",
-    #     place_X, " ", place_Y, "\n")
+        printloc()
+print("U heeft uw bestemming bereikt")
